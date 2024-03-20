131d956f2cc7312e966ea2519bb914f064086b2e
Naushad Akhtar <110463188+Naushadquery@users.noreply.github.com>
2024-03-20 15:10:50 +0530

    Add files via upload

--- EmployeeView.java
+++ EmployeeView.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class EmployeeView {
 
 	private Scanner scanner = new Scanner(System.in);
-
+	private int empCount = 4;
 	private Employee[] employees = new Employee[50];
 	{
 		Employee employee1 = new Employee(1, "nausha", 55);
@@ -19,25 +19,32 @@ public class EmployeeView {
 	}
 
 	public EmployeeView() {
-		System.out.println("      Employee Management System");
-		mainOptions();
-
+		System.out.println("     Employee Management System");
+		while (true) {
+			System.out.println("Entering Main Options");
+			mainOptions();
+			System.out.println("End of some option");
+		}
 	}
 
 	private void mainOptions() {
 		System.out.println("Enter the option from below : ");
 		System.out.println("1. Add Employee");
-		System.out.println("2. Display Employee");
+		System.out.println("2. Display All Employee");
+		System.out.println("3. Display Employee with id :");
+		System.out.println("4. Update Employee");
+		System.out.println("5. Delete Employee");
 		System.out.println("6. Exit Application");
 		int option = scanner.nextInt();
 		switch (option) {
 		case 1: {
 			addEmployees();
-			mainOptions();
+			System.out.println("add employee completeed");
+			return;
 		}
 		case 2: {
 			displayEmployees(employees);
-			mainOptions();
+			return;
 		}
 		case 6: {
 			System.err.println("End of Application.....");
@@ -50,7 +57,26 @@ public class EmployeeView {
 	}
 
 	private void addEmployees() {
-		System.out.println("Employees Added....");
+		boolean ans = true;
+		do {
+			System.out.println("Enter Employee Id : ");
+			int id = scanner.nextInt();
+			System.out.println("Enter Employee Name : ");
+			scanner.nextLine();
+			String name = scanner.nextLine();
+			System.out.println("Enter Employee Age : ");
+			int age = scanner.nextInt();
+			Employee employee = new Employee(id, name, age);
+			employees[empCount] = employee;
+			empCount++;
+			System.out.println("Employees Added....");
+			System.out.println("Do you want to continue..... y/n");
+			String input = scanner.next();
+			if (!input.equalsIgnoreCase("y")) {
+				ans = false;
+			}
+		} while (ans);
+		return;
 	}
 
 	private void displayEmployees(Employee... employees) {
